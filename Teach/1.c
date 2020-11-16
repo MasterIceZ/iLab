3be8f03abe8a62fe201e03144351bf526d16a9c9
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-16 11:45:35 +0700

    Teach Day1

---
+++ Teach/1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+
+///stdio => standard input output
+
+/**
+    int => integer %d
+    long long => long integer %lld
+    float => floating point %f
+    double => real number %lf
+    char => 1 charactor%c
+*/
+
+/**
+    + plus
+    - minus
+    * multiply
+    / divide
+    % mod 
+*/
+
+/**
+    sqrt(a) square-root a
+    pow(a,b) a^b
+*/
+
+
+int main (){
+    printf("A\nB");
+    return 0;
+}
